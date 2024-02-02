7e9caab4ee44466f7b88132c07395aca9ca06f52
DanpoongE <hwangeunbi0121@gmail.com>
2024-02-02 10:24:55 +0900

    update2

--- 240130/SWEA/6485/6485.py
+++ 240130/SWEA/6485/6485.py
@@ -2,19 +2,27 @@
 # 버스 노선은 N개.
 # i번째는 Ai<= <=Bi인 모든 정류장 다니는 버스.
 # p개의 버스 정류장. 각 정류장에 몇 개의 버스 노선이 다니는가.
+# C1, C2, C3..가 차례대로 1번, 2번, 3번 버정이라는 보장이 없으므로 COUNTS[Cj]로 읽도록 하자
 
 T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    counts = [0]*5001           # 1-5000번 노선을 index 0-4999로 하기 귀찮으므로 index하나 늘리고, 0번은 버리기
+    for i in range(N):
+        A, B = map(int, input().split())
+        for j in range(A, B+1): # 1<=A<=B<=5000 늘 출발지정류장 번호가 도착점보다 작으므로 가능
+            counts[j] += 1
+    P = int(input())
+    busstop = [int(input()) for _ in range(P)]
+    print(f'#{tc}', end= ' ')
 
-for tc in range(T):
-    route = int(input())
-    bus = []
-    for where in range(route):
-        start, end = map(int, input().split())
-
+    for i in busstop: # 출력할 정류장 번호
+        print(counts[i], end= ' ')
+    print()
         # str사이의 공백은 split할 때 사라짐.
         # 노선 시작 정류장, 끝 정류장 찾음 1,3 / 2,5
         # 이 범위에 있는 숫자의 index-1 의 list값을 하나씩 증가시키고 싶은데...
-    print(bus)
+
     # P = int(input())
     # count_list = [0]*P
 
--- 240131/SWEA/1979.word/1979.py
+++ 240131/SWEA/1979.word/1979.py
@@ -2,62 +2,34 @@ T = int(input())
 
 for t in range(1, T+1):
     N, K = list(map(int, input().split()))
-    array = []
-    for _ in range(N):
-        arr = list(map(int, input().split()))
-        a =''.join(str(s) for s in arr)
-        array.append(a)
-    print(array)
-    #arr = [list(''.join(input().split())) for _ in range(N)]
-    # a = ''.join(arr)
-    #print(arr)
+    arr=[list(map(int, input().split())) for _ in range(N)]
 
-#     result = 0
-#
-#     for i in range(N):
-#         print(a)
-# # 행을 str으로 바꾸어 0을 구분자로 split. 다시 list에 담아서 len이 K면 result+=1
+    result = 0
 
+    # 행에서 연속된 1의 갯수 찾기
+    for i in range(N):
+        counts = 0
+        for j in range(N):    # (0,0), (0,1), (0,2)...
+            if arr[i][j] == 1:
+                counts += 1
+            else: # 1이 아닌 수가 나오면 그 전까지 3이었을 경우 result값 1개 오르고, counts 재카운트
+                if counts == K:
+                    result +=1
+                counts = 0
+        if counts == K:
+            result += 1
 
+    # 열에서 연속된 1의 갯수 찾기
+    for j in range(N):
+        counts = 0
+        for i in range(N):
+            if arr[i][j] == 1:
+                counts += 1
+            else:
+                if counts == K:
+                    result +=1
+                counts = 0
+        if counts == K:
+            result += 1
 
-
-
-
-
-
-
-
-
-
-
-
-
-    # result = 0
-    #
-    # # 행에서 연속된 1의 갯수 찾기
-    # for i in range(N):
-    #     counts = 0
-    #     for j in range(N):    # (0,0), (0,1), (0,2)...
-    #         if arr[i][j] == 1:
-    #             counts += 1
-    #         else: # 1이 아닌 수가 나오면 그 전까지 3이었을 경우 result값 1개 오르고, counts 재카운트
-    #             if counts == K:
-    #                 result +=1
-    #             counts = 0
-    #     if counts == K:
-    #         result += 1
-    #
-    # # 열에서 연속된 1의 갯수 찾기
-    # for j in range(N):
-    #     counts = 0
-    #     for i in range(N):
-    #         if arr[i][j] == 1:
-    #             counts += 1
-    #         else:
-    #             if counts == K:
-    #                 result +=1
-    #             counts = 0
-    #     if counts == K:
-    #         result += 1
-
-    # print(f'#{t} {result}')
+    print(f'#{t} {result}')
