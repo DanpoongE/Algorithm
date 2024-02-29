c389b2fa8b4560849ede9bfd18edf94ca1bd388a
DanpoongE <hwangeunbi0121@gmail.com>
2024-02-29 17:49:37 +0900

    update29

--- BJ/10163/B10163.py
+++ BJ/10163/B10163.py
@@ -1,19 +1,17 @@
 N = int(input())  # 색종이 장수
 board = [[0] * 1001 for _ in range(1001)]
 
-for _ in range(1, N + 1):
+for paper_num in range(1, N + 1):
     x, y, w, h = list(map(int, input().split()))  # 직사각형의 왼쪽아래 x,y와 가로 세로길이
 
     for r in range(y, y + h):
         for c in range(x, x + w):
-            board[r][c] += 1
+            board[r][c] = paper_num
 
-result = []
+result = [0] * (N + 1)
 for n in range(1, N+1):
-    cnt = 0
     for i in range(1001):
-        cnt += board[i].count(n)
+        result[n] += board[i].count(n)
 
-    result.append(cnt)
-
+result.remove(result[0])
 print(*result, sep='\n')
